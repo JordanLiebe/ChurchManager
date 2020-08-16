07ada091072dda19d19a419be83ad1309ee5e384
Jordan M. Liebe <Jordan.Liebe@outlook.com>
2020-08-15 22:10:15 -0700

    Fixed .gitignore and added swagger

--- backend/ChurchManagerApi/Startup.cs
+++ backend/ChurchManagerApi/Startup.cs
@@ -25,15 +25,32 @@ namespace ChurchManagerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Church Manager API");
+                });
+            }
+            else
+            {
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/blog/swagger/v1/swagger.json", "Church Manager API");
+                });
             }
 
             app.UseHttpsRedirection();
